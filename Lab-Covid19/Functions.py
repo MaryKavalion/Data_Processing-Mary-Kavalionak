8507cba80fc45b05c1cbd144ae54a0dc9ac7e421
Mar-kav <97294723+Mar-kav@users.noreply.github.com>
2023-11-08 18:02:53 +0100

    new file:   Lab-Covid19/Part 2. Warming_up_vaccination.ipynb

--- Lab-Covid19/Functions.py
+++ Lab-Covid19/Functions.py
@@ -4,3 +4,10 @@ def make_ticks(list_to_shorten, n):
         if (i+1) % n == 0:
             ticks.append(list_to_shorten[i])
     return ticks
+
+def count_and_check(text, dataset, column_name, second_column_name):
+    if dataset[column_name].nunique() == dataset[second_column_name].nunique():
+        number = dataset[column_name].nunique()
+        print (text, number)
+    else:
+        print ("Your data needs cleaning")
